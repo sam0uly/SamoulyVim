8fe34dea602a4a9dfaacdf69480ed259951fd1d0
N1xev <alasamouly@gmail.com>
2026-08-17 10:38:58 +0300

    feat(term): rework toggleterm keymaps and options

--- lua/plugins/tools/term.lua
+++ lua/plugins/tools/term.lua
@@ -1,108 +1,98 @@
 return {
   {
     "folke/snacks.nvim",
-    opts = {
-      terminal = {
-        enabled = false,
-      },
-    },
+    enabled = true,
   },
   {
     "akinsho/toggleterm.nvim",
-    lazy = true,
-    cmd = { "ToggleTerm" },
-    keys = {
-      {
-        "<leader>Tf",
-        function()
-          local count = vim.v.count1
-          require("toggleterm").toggle(count, 0, LazyVim.root.get(), "float")
-        end,
-        desc = "ToggleTerm (float root_dir)",
-      },
-      {
-        "<leader>Th",
-        function()
-          local count = vim.v.count1
-          require("toggleterm").toggle(count, 15, LazyVim.root.get(), "horizontal")
-        end,
-        desc = "ToggleTerm (horizontal root_dir)",
-      },
-      {
-        "<leader>Tv",
-        function()
-          local count = vim.v.count1
-          require("toggleterm").toggle(count, vim.o.columns * 0.4, LazyVim.root.get(), "vertical")
-        end,
-        desc = "ToggleTerm (vertical root_dir)",
-      },
-      {
-        "<leader>Tn",
-        "<cmd>ToggleTermSetName<cr>",
-        desc = "Set term name",
-      },
-      {
-        "<leader>Ts",
-        "<cmd>TermSelect<cr>",
-        desc = "Select term",
-      },
-      {
-        "<leader>Tt",
-        function()
-          require("toggleterm").toggle(1, 100, LazyVim.root.get(), "tab")
-        end,
-        desc = "ToggleTerm (tab root_dir)",
-      },
-      {
-        "<leader>TT",
-        function()
-          require("toggleterm").toggle(1, 100, vim.fn.getcwd(), "tab")
-        end,
-        desc = "ToggleTerm (tab cwd_dir)",
-      },
-    },
     opts = {
-      -- size can be a number or function which is passed the current terminal
-      size = function(term)
-        if term.direction == "horizontal" then
-          return 15
-        elseif term.direction == "vertical" then
-          return vim.o.columns * 0.4
-        end
-      end,
-      open_mapping = [[<C-\>]],
-      -- on_open = fun(t: Terminal), -- function to run when the terminal opens
-      -- on_close = fun(t: Terminal), -- function to run when the terminal closes
-      -- on_stdout = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stdout
-      -- on_stderr = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stderr
-      -- on_exit = fun(t: Terminal, job: number, exit_code: number, name: string) -- function to run when terminal process exits
-      hide_numbers = true, -- hide the number column in toggleterm buffers
-      shade_filetypes = {},
-      shade_terminals = true,
-      -- shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+      insert_mappings = true,
+      terminal_mappings = true,
       start_in_insert = true,
-      insert_mappings = true, -- whether or not the open mapping applies in insert mode
-      terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
-      persist_size = true,
-      direction = "horizontal" or "vertical" or "window" or "float",
-      -- direction = "vertical",
-      close_on_exit = true, -- close the terminal window when the process exits
-      shell = vim.o.shell, -- change the default shell
-      -- This field is only relevant if direction is set to 'float'
-      float_opts = {
-        -- The border key is *almost* the same as 'nvim_open_win'
-        -- see :h nvim_open_win for details on borders however
-        -- the 'curved' border is a custom border type
-        -- not natively supported but implemented in this plugin.
-        border = "single" or "double" or "shadow" or "curved",
-        -- width = <value>,
-        -- height = <value>,
-        winblend = 3,
-        highlights = {
-          border = "Normal",
-          background = "Normal",
-        },
+      persist_mode = true,
+      shade_terminals = false,
+    },
+
+    keys = {
+      { "<leader>td", false },
+
+      -- New terminals
+      {
+        "<leader>th",
+        function()
+          require("toggleterm.terminal").Terminal
+            :new({
+              direction = "horizontal",
+              display_name = "Horizontal",
+            })
+            :open()
+        end,
+        desc = "New Horizontal",
+      },
+      {
+        "<leader>tv",
+        function()
+          require("toggleterm.terminal").Terminal
+            :new({
+              direction = "vertical",
+              display_name = "Vertical",
+            })
+            :open()
+        end,
+        desc = "New Vertical",
+      },
+
+      -- Toggle
+      {
+        "<leader>tH",
+        "<cmd>ToggleTerm direction=horizontal<CR>",
+        desc = "Toggle Horizontal",
+      },
+      {
+        "<leader>tV",
+        "<cmd>ToggleTerm direction=vertical<CR>",
+        desc = "Toggle Vertical",
+      },
+
+      -- Instance picker
+      {
+        "<leader>tt",
+        "<cmd>TermSelect<CR>",
+        desc = "Terminal List",
+      },
+
+      -- Kill focused
+      {
+        "<leader>tk",
+        function()
+          local ok, Terminal = pcall(require, "toggleterm.terminal")
+          if not ok then
+            return
+          end
+
+          local id = vim.b.toggle_number
+          if not id then
+            vim.notify("Not inside a ToggleTerm", vim.log.levels.WARN)
+            return
+          end
+
+          local term = Terminal.get(id)
+          if term then
+            term:shutdown()
+          end
+        end,
+        desc = "Kill Terminal",
       },
     },
+
+    config = function(_, opts)
+      require("toggleterm").setup(opts)
+
+      -- Double Esc -> Normal mode
+      vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], {
+        desc = "Terminal Normal Mode",
+        silent = true,
+      })
+    end,
   },
 }
